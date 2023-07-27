0b16bc3ccb25fb9c7f8778f7fd3d4c1fe384ed9d
Petr Kotov <ipetrkotov@gmaiil.com>
2023-07-27 18:25:05 +0300

    1)в методе onAuthenticationSuccess() сделал для админов с ролью юзер и только для админов редирект на "/admin" . 2)WebSecurityConfig, AdminController, UserController сделал зависимость от интерфейса. 3)Добавил названиям методов информативности

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -16,11 +16,9 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("ROLE_ADMIN") && !roles.contains("ROLE_USER")) { //только админ
+        if (roles.contains("ROLE_ADMIN")) {
             httpServletResponse.sendRedirect("/admin");
-        } else if (roles.contains("ROLE_ADMIN") && roles.contains("ROLE_USER")) { // и админ и юзер
-            httpServletResponse.sendRedirect("/admin");
-        } else if (roles.contains("ROLE_USER")) { //просто юзер
+        } else if (roles.contains("ROLE_USER")) {
             httpServletResponse.sendRedirect("/user");
         }
     }
